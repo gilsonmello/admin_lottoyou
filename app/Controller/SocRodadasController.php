79a624eea1d1576a09b132d5e2a97bcf811f66c3
Gilson Mello <junnyorr.sirnandes@gmail.com>
2018-06-03 18:29:01 -0300

    atualizações

--- app/Controller/SocRodadasController.php
+++ app/Controller/SocRodadasController.php
@@ -645,11 +645,10 @@ class SocRodadasController extends AppController {
                 if($qtd_acertos_placares > 0) {
                     //$acertos_placares_peso = ($qtd_acertos_placares + $qtd_acertos_diferenca_gols_ou_empates);
                     $criterio += $qtd_acertos_placares;
-                } 
-                
+                }                 
 
                 if($qtd_pontuacao_bola_ouro > 0) {
-                    $criterio += $qtd_pontuacao_bola_ouro;
+                    $criterio += $qtd_pontuacao_bola_ouro + $qtd_acertos_diferenca_gols_ou_empates + $qtd_acertos_placares;
                 }
                 
                 //$pontuacao_bola_ouro_peso = $qtd_pontuacao_bola_ouro + $qtd_acertos_placares +    $qtd_acertos_diferenca_gols_ou_empates;
