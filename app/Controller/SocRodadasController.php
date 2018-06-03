4a00badae57f91b1b4837b1cd23e0261f3ee4dff
Gilson Mello <junnyorr.sirnandes@gmail.com>
2018-06-03 19:28:21 -0300

    atualizações

--- app/Controller/SocRodadasController.php
+++ app/Controller/SocRodadasController.php
@@ -639,20 +639,17 @@ class SocRodadasController extends AppController {
 
 
                 if($qtd_acertos_diferenca_gols_ou_empates > 0) {
-                    $criterio += $qtd_acertos_diferenca_gols_ou_empates;
+                    $criterio = $qtd_acertos_diferenca_gols_ou_empates;
                 }
                 
                 if($qtd_acertos_placares > 0) {
                     //$acertos_placares_peso = ($qtd_acertos_placares + $qtd_acertos_diferenca_gols_ou_empates);
-                    $criterio += $qtd_acertos_placares + $qtd_acertos_diferenca_gols_ou_empates;
+                    $criterio = $qtd_acertos_placares + $qtd_acertos_diferenca_gols_ou_empates;
                 }     
-
-                if($qtd_pontuacao_bola_ouro > 0) {
-                    //$acertos_placares_peso = ($qtd_acertos_placares + $qtd_acertos_diferenca_gols_ou_empates);
-                    $criterio += $pontuacao + $qtd_acertos_placares + $qtd_acertos_diferenca_gols_ou_empates;
-                } else {
-                    $criterio += $pontuacao_sem_bola_ouro + $qtd_acertos_placares + $qtd_acertos_diferenca_gols_ou_empates;
-                }            
+                
+                //$acertos_placares_peso = ($qtd_acertos_placares + $qtd_acertos_diferenca_gols_ou_empates);
+                //$criterio += $pontuacao_sem_bola_ouro;
+                            
 
                 
                 //$criterio += ($qtd_acertos_diferenca_gols_ou_empates + $qtd_acertos_placares) * 3;
