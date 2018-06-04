1fdce3a4545ff4d6db5f366d0efe0502d9227831
Gilson Mello <junnyorr.sirnandes@gmail.com>
2018-06-04 12:09:12 -0300

    atualizações

--- app/Controller/SocRodadasController.php
+++ app/Controller/SocRodadasController.php
@@ -711,6 +711,18 @@ class SocRodadasController extends AppController {
                         }
                     }
                 }
+
+                for($i = 0; $i < count($aposta_empates); $i++) {
+                    for($j = $i + 1; $j < count($aposta_empates); $j++) {
+                        if($aposta_empates[$i]['SocAposta']['qtd_acertos_diferenca_gols_ou_empate'] < $aposta_empates[$j]['SocAposta']['qtd_acertos_diferenca_gols_ou_empate']) {
+                            $aux = $aposta_empates[$i];
+                            $aposta_empates[$i] = $aposta_empates[$j];
+                            $aposta_empates[$j] = $aux;
+                            $aposta_empates[$j]['SocAposta']['total_pontuacao'] += 1;
+                            $this->SocAposta->save($aposta_empates[$j]);
+                        }
+                    }
+                }
             }
 
 
