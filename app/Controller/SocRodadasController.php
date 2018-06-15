fc17842771202290ea4acf1e5b955f835261d022
Gilson Mello <junnyorr.sirnandes@gmail.com>
2018-06-15 11:38:28 -0300

    atualizações

--- app/Controller/SocRodadasController.php
+++ app/Controller/SocRodadasController.php
@@ -1312,6 +1312,7 @@ class SocRodadasController extends AppController {
                             if($aposta_empates[$i]['SocAposta']['pontuacao_bola_ouro'] < $aposta_empates[$j]['SocAposta']['pontuacao_bola_ouro']) {
                                 $aposta_empates[$j]['SocAposta']['total_pontuacao'] += 1;
                                 $this->SocAposta->save($aposta_empates[$j]);
+                                continue;
                             }
 
                             /*if($aposta_empates[$i]['SocAposta']['pontuacao_bola_ouro'] > $aposta_empates[$j]['SocAposta']['pontuacao_bola_ouro']) {
@@ -1359,6 +1360,7 @@ class SocRodadasController extends AppController {
                             if($aposta_empates[$i]['SocAposta']['qtd_acertos_placares'] < $aposta_empates[$j]['SocAposta']['qtd_acertos_placares']) {
                                 $aposta_empates[$j]['SocAposta']['total_pontuacao'] += 1;
                                 $this->SocAposta->save($aposta_empates[$j]);
+                                continue;
                             }
 
                             /*if($aposta_empates[$i]['SocAposta']['qtd_acertos_placares'] > $aposta_empates[$j]['SocAposta']['qtd_acertos_placares']) {
@@ -1406,6 +1408,7 @@ class SocRodadasController extends AppController {
                             if($aposta_empates[$i]['SocAposta']['qtd_acertos_diferenca_gols_ou_empate'] < $aposta_empates[$j]['SocAposta']['qtd_acertos_diferenca_gols_ou_empate']) {
                                 $aposta_empates[$j]['SocAposta']['total_pontuacao'] += 1;
                                 $this->SocAposta->save($aposta_empates[$j]);
+                                continue;
                             }
 
                             /*if($aposta_empates[$i]['SocAposta']['qtd_acertos_diferenca_gols_ou_empate'] > $aposta_empates[$j]['SocAposta']['qtd_acertos_diferenca_gols_ou_empate']) {
